f5da09f1de14ceac842c69b4b7ad854f9f485c5f
shun-honda <hondashun@honda-no-MacBook-Pro.local>
2015-03-02 19:18:49 +0900

    add new NezVM

--- run_pegvm.sh
+++ run_pegvm.sh
@@ -15,3 +15,15 @@ echo "\nparse xmark5m.xml\n"
 
 echo "\nparse nss_cache.c\n"
 ./$VMDIR/pegvm -t stat -p $BYTECODE/c99.bin -i $DATADIR/nss_cache.c
+
+echo "\nparse earthquake.geojson\n"
+./$VMDIR/pegvm -t stat -p $BYTECODE/json_noObj.bin -i $DATADIR/earthquake.geojson
+
+echo "\nparse benchmark4.json\n"
+./$VMDIR/pegvm -t stat -p $BYTECODE/json_noObj.bin -i $DATADIR/benchmark4.json
+
+echo "\nparse xmark5m.xml\n"
+./$VMDIR/pegvm -t stat -p $BYTECODE/xml_noObj.bin -i $DATADIR/xmark5m.xml
+
+echo "\nparse nss_cache.c\n"
+./$VMDIR/pegvm -t stat -p $BYTECODE/c99_noObj.bin -i $DATADIR/nss_cache.c
