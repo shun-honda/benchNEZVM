0ebcfd749b5f5cebc3d906b7a4f9136b9cc835d4
shun-honda <shun.honda.suffix@gmail.com>
2015-05-17 14:55:00 +0900

    update bench shell script

--- benchMinivm.sh
+++ benchMinivm.sh
@@ -5,36 +5,6 @@ BYTECODEOPT=minivmByteCode/opt
 VMDIR=../MiniNezVM/src/mini_instruction_vm/build
 OPTVMDIR=../MiniNezVM/src/mini_instruction_opt_vm/build
 
-echo "\nunoptimized bench data\n"
-
-make -C ../MiniNezVM/src/mini_instruction_vm/build clean
-make -C ../MiniNezVM/src/mini_instruction_vm/build
-echo "\nparse earthquake.geojson\n"
-./../MiniNezVM/src/mini_instruction_vm/build/nezvm -t stat -p minivmByteCode/nonOpt/json.bin -i input/earthquake.geojson
-
-echo "\nparse benchmark4.json\n"
-./../MiniNezVM/src/mini_instruction_vm/build/nezvm -t stat -p minivmByteCode/nonOpt/json.bin -i input/benchmark4.json
-
-echo "\nparse citys.json\n"
-./../MiniNezVM/src/mini_instruction_vm/build/nezvm -t stat -p minivmByteCode/nonOpt/json.bin -i input/citys.json
-
-echo "\nparse xmark5m.xml\n"
-./../MiniNezVM/src/mini_instruction_vm/build/nezvm -t stat -p minivmByteCode/nonOpt/xml.bin -i input/xmark5m.xml
-
-echo "\nparse geometry_base.xml\n"
-./../MiniNezVM/src/mini_instruction_vm/build/nezvm -t stat -p minivmByteCode/nonOpt/xml.bin -i input/geometry_base.xml
-
-echo "\nparse 01HOKKAI_ROME.CSV\n"
-./../MiniNezVM/src/mini_instruction_vm/build/nezvm -t stat -p minivmByteCode/nonOpt/csv.bin -i input/01HOKKAI_ROME.CSV
-
-echo "\nparse earthquake.csv\n"
-./../MiniNezVM/src/mini_instruction_vm/build/nezvm -t stat -p minivmByteCode/nonOpt/csv.bin -i input/earthquake.csv
-
-echo "\nparse test.syslog\n"
-./../MiniNezVM/src/mini_instruction_vm/build/nezvm -t stat -p minivmByteCode/nonOpt/syslog.bin -i input/test.syslog
-
-echo "\noptimized bench data\n"
-
 make -C ../MiniNezVM/src/mini_instruction_opt_vm/build clean
 make -C ../MiniNezVM/src/mini_instruction_opt_vm/build
 echo "\nparse earthquake.geojson\n"
